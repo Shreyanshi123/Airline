ede7f6d3a2d47673c352f039e5160c61daa410a2
Aster <shreyanshimishra7689@gmail.com>
2025-05-08 15:25:22 +0530

    "booking component completely running successfully"

--- src/app/components/booking/booking.component.ts
+++ src/app/components/booking/booking.component.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-booking',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './booking.component.html',
   styleUrl: './booking.component.css'
 })
 export class BookingComponent {
-
+  travellerList:any;
+  flightDetails:any;
+  tripDetails:any;
 }
